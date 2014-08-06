00195ec00b148f1d97b6148fb8e4088cb2208711
Carlos Lozano <carlos@adaptive.me>
2014-08-06 13:34:40 +0200

    Objective-C project.

---
+++ adaptive-arp-api-objc/adaptive-arp-api-objc/adaptive_arp_api_objc.h
@@ -0,0 +1,13 @@
+//
+//  adaptive_arp_api_objc.h
+//  adaptive-arp-api-objc
+//
+//  Created by Carlos Lozano Diez on 06/08/2014.
+//  Copyright (c) 2014 Adaptive. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface adaptive_arp_api_objc : NSObject
+
+@end
---
+++ adaptive-arp-api-objc/adaptive-arp-api-objc/adaptive_arp_api_objc.m
@@ -0,0 +1,13 @@
+//
+//  adaptive_arp_api_objc.m
+//  adaptive-arp-api-objc
+//
+//  Created by Carlos Lozano Diez on 06/08/2014.
+//  Copyright (c) 2014 Adaptive. All rights reserved.
+//
+
+#import "adaptive_arp_api_objc.h"
+
+@implementation adaptive_arp_api_objc
+
+@end
