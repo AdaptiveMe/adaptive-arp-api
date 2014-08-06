68de070f58d9a22a5d719f6d6fb7be8568c75c4d
Carlos Lozano <carlos@adaptive.me>
2014-08-06 18:23:06 +0200

    Sharpen tool for C#.

---
+++ tools/osx/sharpen/sharpen.sh
@@ -0,0 +1,31 @@
+. `pwd`/config.make
+
+JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_21.jdk/Contents/Home
+DIRECTORY=$1
+PROJECT=$2
+SOURCES=$3
+OPTIONS=`pwd`/$4
+OUTDIR=`pwd`/$5
+HEADER=$6
+TEMPDIR=build
+
+rm -rf $DIRECTORY/$TEMPDIR
+mkdir -p $DIRECTORY/$TEMPDIR/$PROJECT
+cp -r $DIRECTORY/$SOURCES $DIRECTORY/$TEMPDIR/$PROJECT
+pushd $DIRECTORY
+
+
+$JAVA_HOME/bin/java \
+		-Xms256m \
+		-Xmx512m \
+		-Dosgi.requiredJavaVersion=1.7 \
+		-Declipse.consoleLog=true \
+		-cp $ECLIPSE_PATH/plugins/org.eclipse.equinox.launcher_*.jar \
+		org.eclipse.core.launcher.Main \
+		-data $TEMPDIR \
+		-application sharpen.core.application \
+		$PROJECT/$SOURCES @$OPTIONS -header $prefix/$HEADER
+
+popd
+mkdir -p $OUTDIR
+cp -r $DIRECTORY/$TEMPDIR/$PROJECT.net/$SOURCES/* $OUTDIR
