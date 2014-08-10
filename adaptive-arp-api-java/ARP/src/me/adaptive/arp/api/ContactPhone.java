4fa88dfa042240ba91913785a1412edb5551c068
kechis <kechis@gmail.com>
2014-08-10 10:59:35 +0200

    javadoc finished

--- adaptive-arp-api-java/ARP/src/me/adaptive/arp/api/ContactPhone.java
+++ adaptive-arp-api-java/ARP/src/me/adaptive/arp/api/ContactPhone.java
@@ -21,7 +21,9 @@
  *
  * Contributors:
  *
- *     *
+ *     * Francisco Javier Martin Bueno
+ *             <https://github.com/kechis>
+ *             <mailto:kechis@gmail.com>
  *
  * =====================================================================================================================
  */
@@ -34,4 +36,70 @@ import java.io.Serializable;
  * Created by clozano on 08/08/14.
  */
 public class ContactPhone implements Serializable {
+    /**
+     * The destination of the phone
+     * @since ARP1.0
+     */
+    enum Description {
+        Mobile, Work, Home, Main, HomeFax, WorkFax, Other
+    }
+
+    /**
+     * The phone number
+     * @since ARP1.0
+     */
+    private string phone;
+
+    /**
+     * The phone number description
+     * @since ARP1.0
+     */
+    private Description description;
+
+    /**
+     * Constructor used by implementation to set the contact Phone
+     * @param phone number
+     * @param description of the phone number
+     * @since ARP1.0
+     */
+    public ContactPhone(string phone, Description description) {
+        this.phone = phone;
+        this.description = description;
+    }
+
+    /**
+     * Returns the phone number
+     * @return phone number
+     * @since ARP1.0
+     */
+    public string getPhone() {
+        return phone;
+    }
+
+    /**
+     * Set the phone number
+     * @param phone number
+     * @since ARP1.0
+     */
+    public void setPhone(string phone) {
+        this.phone = phone;
+    }
+
+    /**
+     * Returns the phone description
+     * @return description of the phone number
+     * @since ARP1.0
+     */
+    public me.adaptive.arp.api.ContactPhone.Description getDescription() {
+        return description;
+    }
+
+    /**
+     * Set the description of the phone number
+     * @param description of the phone number
+     * @since ARP1.0
+     */
+    public void setDescription(me.adaptive.arp.api.ContactPhone.Description description) {
+        this.description = description;
+    }
 }
