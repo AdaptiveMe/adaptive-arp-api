f7b59c907469f66ba4974af4e824299c43aad579
Carlos Lozano <carlos@adaptive.me>
2014-08-28 00:38:36 +0200

    Auto-generate stubs in Swift from Java (In Progress)

---
+++ adaptive-arp-api-swift/ARP/IBaseMedia.swift
@@ -0,0 +1,17 @@
+//
+//  Auto-generated from: me.adaptive.arp.api.IBaseMedia
+//
+//  IBaseMedia.swift
+//
+//  Created by Carlos Lozano Diez on 27 Aug 2014 22:34:50 GMT.
+//  Copyright (c) 2014 Carlos Lozano Diez. All rights reserved.
+//
+//  *** WORK IN PROGRESS ***
+//
+
+import Foundation
+
+public protocol IBaseMedia : IAdaptiveRP {
+
+
+}
---
+++ adaptive-arp-api-swift/ARP/IInternalStorage.swift
@@ -0,0 +1,17 @@
+//
+//  Auto-generated from: me.adaptive.arp.api.IInternalStorage
+//
+//  IInternalStorage.swift
+//
+//  Created by Carlos Lozano Diez on 27 Aug 2014 22:34:50 GMT.
+//  Copyright (c) 2014 Carlos Lozano Diez. All rights reserved.
+//
+//  *** WORK IN PROGRESS ***
+//
+
+import Foundation
+
+public protocol IInternalStorage : IStore {
+
+
+}
