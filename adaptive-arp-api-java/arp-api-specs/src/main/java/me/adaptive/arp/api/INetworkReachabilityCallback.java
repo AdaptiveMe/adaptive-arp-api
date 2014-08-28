7fbd19105ce44b80ecd038bc74c3803734c8c8f8
Francisco Javier Martin <frmi@gft.com>
2014-08-28 15:22:36 +0200

    New listeners

---
+++ adaptive-arp-api-java/arp-api-specs/src/main/java/me/adaptive/arp/api/INetworkReachabilityCallback.java
@@ -0,0 +1,63 @@
+/*
+ * =| ADAPTIVE RUNTIME PLATFORM |=======================================================================================
+ *
+ * (C) Copyright 2013-2014 Carlos Lozano Diez t/a Adaptive.me <http://adaptive.me>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ * Original author:
+ *
+ *     * Carlos Lozano Diez
+ *                 <http://github.com/carloslozano>
+ *                 <http://twitter.com/adaptivecoder>
+ *                 <mailto:carlos@adaptive.me>
+ *
+ * Contributors:
+ *
+ *     *
+ *
+ * =====================================================================================================================
+ */
+
+package me.adaptive.arp.api;
+
+/**
+ * Created by FRMI on 28/08/2014.
+ */
+public interface INetworkReachabilityCallback {
+    /**
+     * Correct data received.
+     *
+     * @param result
+     * @since ARP1.0
+     */
+    void onResult(boolean result);
+
+    /**
+     * Data received with warning - ie Found entries with existing key and values have been overriden
+     *
+     * @param result
+     * @param warning
+     * @since ARP1.0
+     */
+    void onWarning(boolean result, Warning warning);
+
+    /**
+     * No data received - error condition, not authorized .
+     *
+     * @param error
+     * @since ARP1.0
+     */
+    void onError(Error error);
+
+    enum Warning {IncorrectScheme}
+
+    enum Error {NoPermission, NetworkOnMainThreadException}
+}
