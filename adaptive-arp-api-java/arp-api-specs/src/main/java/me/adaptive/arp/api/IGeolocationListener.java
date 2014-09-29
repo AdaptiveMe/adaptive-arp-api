234cd138310ad4333f2f405d0a78cbf57eff0006
Ferran Vilà Conesa <fnva@users.noreply.github.com>
2014-09-29 16:01:20 +0200

    Update IGeolocationListener.java

--- adaptive-arp-api-java/arp-api-specs/src/main/java/me/adaptive/arp/api/IGeolocationListener.java
+++ adaptive-arp-api-java/arp-api-specs/src/main/java/me/adaptive/arp/api/IGeolocationListener.java
@@ -56,7 +56,7 @@ public interface IGeolocationListener extends IBaseListener {
      */
     void onError(Error error);
 
-    public enum Error {}
+    public enum Error {RestrictedAccess, DeniedAccess, StatusNotDetermined}
 
     public enum Warning {HighDoP}
 }
