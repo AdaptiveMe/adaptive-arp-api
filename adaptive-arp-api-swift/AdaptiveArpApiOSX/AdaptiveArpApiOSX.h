4b36e95e17c6ece70a373a1889baf7793b410ed7
Carlos Lozano <carlos@adaptive.me>
2014-10-30 18:10:42 +0100

    Project configuration update.

---
+++ adaptive-arp-api-swift/AdaptiveArpApiOSX/AdaptiveArpApiOSX.h
@@ -0,0 +1,19 @@
+//
+//  AdaptiveArpApiOSX.h
+//  AdaptiveArpApiOSX
+//
+//  Created by Carlos Lozano on 30/10/14.
+//  Copyright (c) 2014 Carlos Lozano Diez. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for AdaptiveArpApiOSX.
+FOUNDATION_EXPORT double AdaptiveArpApiOSXVersionNumber;
+
+//! Project version string for AdaptiveArpApiOSX.
+FOUNDATION_EXPORT const unsigned char AdaptiveArpApiOSXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AdaptiveArpApiOSX/PublicHeader.h>
+
+
