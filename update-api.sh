bdbdfde1b95f91d7617d07b1ba0958aeda87a349
Carlos Lozano <carlos@adaptive.me>
2014-12-16 19:30:46 +0100

    Bump version.

--- update-api.sh
+++ update-api.sh
@@ -1,3 +1,5 @@
-rm -Rf adaptive-arp-api-java/arp-api-java/src/main/java 
+# Java API, update sources.
+rm -Rf adaptive-arp-api-java/arp-api-java/src/main/java
 cp -R out/JavaValidation/src/main/java adaptive-arp-api-java/arp-api-java/src/main
 rm -Rf adaptive-arp-api-java/arp-api-java/src/main/java/me/adaptive/arp/impl
+
