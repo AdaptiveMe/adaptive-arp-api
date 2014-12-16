b841ba3f5d1107cad533a0b04efffcecb26e2826
Carlos Lozano <carlos@adaptive.me>
2014-12-16 19:21:44 +0100

    Remove j2obj tool... heavy-handed.

--- tools/osx/j2objc/compile.sh
+++
@@ -1,7 +0,0 @@
-M_FILE=`echo $1`
-O_FILE=`echo $1 | sed 's/\.m/.o/'`
-COMPILER=$2
-BUILD_DIR=$3
-echo compiling $M_FILE
-$COMPILER -I$BUILD_DIR -c $M_FILE -o $O_FILE
-
--- tools/osx/j2objc/prefixhelper.sh
+++
@@ -1,5 +0,0 @@
-FILE=$1
-DIR="$2/adaptive-arp-api-java/ARP/src"
-DEST=$3
-echo $FILE=AW | sed "s|$DIR||" | sed "s|/|.|g" >> $DEST
-
