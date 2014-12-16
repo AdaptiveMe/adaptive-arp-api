6e7fe2476b395530938f81b5fdab709b294a1a07
Carlos Lozano <carlos@adaptive.me>
2014-12-16 20:39:16 +0100

    Work in progress.

---
+++ out/SwiftValidation/Sources/IMail.swift
@@ -0,0 +1,121 @@
+/**
+--| ADAPTIVE RUNTIME PLATFORM |----------------------------------------------------------------------------------------
+
+(C) Copyright 2013-2015 Carlos Lozano Diez t/a Adaptive.me <http://adaptive.me>.
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 . Unless required by appli-
+-cable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,  WITHOUT
+WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the  License  for the specific language governing
+permissions and limitations under the License.
+
+Original author:
+
+    * Carlos Lozano Diez
+            <http://github.com/carloslozano>
+            <http://twitter.com/adaptivecoder>
+            <mailto:carlos@adaptive.me>
+
+Contributors:
+
+    * Ferran Vila Conesa
+             <http://github.com/fnva>
+             <http://twitter.com/ferran_vila>
+             <mailto:ferran.vila.conesa@gmail.com>
+
+    * See source code files for contributors.
+
+Release:
+
+    * @version v2.0.2
+
+-------------------------------------------| aut inveniam viam aut faciam |--------------------------------------------
+*/
+
+package me.adaptive.arp.api;
+
+import com.google.gson.Gson;
+
+/**
+   Interface for Managing the Mail operations
+   Auto-generated implementation of IMail specification.
+*/
+public class MailBridge extends BasePIMBridge implements IMail, APIBridge {
+
+     /**
+        API Delegate.
+     */
+     private IMail delegate;
+
+     /**
+        Constructor with delegate.
+
+        @param delegate The delegate implementing platform specific functions.
+     */
+     public MailBridge(IMail delegate) {
+          super();
+          this.delegate = delegate;
+     }
+     /**
+        Get the delegate implementation.
+        @return IMail delegate that manages platform specific functions..
+     */
+     public final IMail getDelegate() {
+          return this.delegate;
+     }
+     /**
+        Set the delegate implementation.
+
+        @param delegate The delegate implementing platform specific functions.
+     */
+     public final void setDelegate(IMail delegate) {
+          this.delegate = delegate;
+     }
+
+     /**
+        Send an Email
+
+        @param data     Payload of the email
+        @param callback Result callback of the operation
+        @since ARP1.0
+     */
+     public void sendEmail(Email data, IMessagingCallback callback) {
+          // Start logging elapsed time.
+          long tIn = System.currentTimeMillis();
+          ILogging logger = AppRegistryBridge.getInstance().getLoggingBridge();
+
+          if (logger!=null) logger.log(ILoggingLogLevel.DEBUG, this.apiGroup.name(),this.getClass().getSimpleName()+" executing sendEmail({"+data+"},{"+callback+"}).");
+
+          if (this.delegate != null) {
+               this.delegate.sendEmail(data, callback);
+               if (logger!=null) logger.log(ILoggingLogLevel.DEBUG, this.apiGroup.name(),this.getClass().getSimpleName()+" executed 'sendEmail' in "+(System.currentTimeMillis()-tIn)+"ms.");
+          } else {
+               if (logger!=null) logger.log(ILoggingLogLevel.ERROR, this.apiGroup.name(),this.getClass().getSimpleName()+" no delegate for 'sendEmail'.");
+          }
+          
+     }
+
+     /**
+        Invokes the given method specified in the API request object.
+
+        @param request APIRequest object containing method name and parameters.
+        @return String with JSON response or a zero length string if the response is asynchronous or null if method not found.
+     */
+     public String invoke(APIRequest request) {
+          String responseJSON = "";
+          switch (request.getMethodName()) {
+               case "sendEmail":
+                    Email data0 = this.gson.fromJson(request.getParameters()[0], Email.class);
+                    IMessagingCallback callback0 = new MessagingCallbackImpl(request.getAsyncId());
+                    this.sendEmail(data0, callback0);
+                    break;
+               default:
+                    // 404 - response null.
+                    responseJSON = null;
+          }
+          return responseJSON;
+     }
+}
+/**
+------------------------------------| Engineered with ♥ in Barcelona, Catalonia |--------------------------------------
+*/
