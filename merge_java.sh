1a0fec5ac73ed2fd4f1f2e34c9fd8ab60c289770
Carlos Lozano <carlos@adaptive.me>
2014-12-16 19:27:31 +0100

    Bump version.

--- merge_java.sh
+++
@@ -1,3 +0,0 @@
-rm -Rf adaptive-arp-api-java/arp-api-specs/src/main/java 
-cp -R out/JavaValidation/src/main/java adaptive-arp-api-java/arp-api-specs/src/main
-rm -Rf adaptive-arp-api-java/arp-api-specs/src/main/java/me/adaptive/arp/impl
---
+++ update-api.sh
@@ -0,0 +1,3 @@
+rm -Rf adaptive-arp-api-java/arp-api-java/src/main/java 
+cp -R out/JavaValidation/src/main/java adaptive-arp-api-java/arp-api-java/src/main
+rm -Rf adaptive-arp-api-java/arp-api-java/src/main/java/me/adaptive/arp/impl
