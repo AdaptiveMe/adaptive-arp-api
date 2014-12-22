84119e51f5b1924ad71992ab0bcd389c6491d1cd
Carlos Lozano <carlos@adaptive.me>
2014-12-22 14:12:30 +0100

    Legacy file remove.

--- update-api.sh
+++
@@ -1,5 +0,0 @@
-# Java API, update sources.
-rm -Rf adaptive-arp-api-java/arp-api-java/src/main/java
-cp -R out/JavaValidation/src/main/java adaptive-arp-api-java/arp-api-java/src/main
-rm -Rf adaptive-arp-api-java/arp-api-java/src/main/java/me/adaptive/arp/impl
-
