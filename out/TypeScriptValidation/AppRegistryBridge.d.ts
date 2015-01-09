1180db03451bd42de5b07786ae11b2dfdb8e3352
Carlos Lozano <carlos@adaptive.me>
2015-01-09 14:18:11 +0100

    TypeScript - AppRegistry work in progress.

---
+++ out/TypeScriptValidation/AppRegistryBridge.d.ts
@@ -0,0 +1,108 @@
+/// <reference path="CommonUtil.d.ts" />
+/// <reference path="IAcceleration.d.ts" />
+/// <reference path="IAdaptiveRPGroup.d.ts" />
+/// <reference path="IAds.d.ts" />
+/// <reference path="IAlarm.d.ts" />
+/// <reference path="IAmbientLight.d.ts" />
+/// <reference path="IAnalytics.d.ts" />
+/// <reference path="IAppRegistry.d.ts" />
+/// <reference path="IAudio.d.ts" />
+/// <reference path="IBarcode.d.ts" />
+/// <reference path="IBarometer.d.ts" />
+/// <reference path="IBluetooth.d.ts" />
+/// <reference path="IBrowser.d.ts" />
+/// <reference path="ICalendar.d.ts" />
+/// <reference path="ICamera.d.ts" />
+/// <reference path="ICapabilities.d.ts" />
+/// <reference path="ICloud.d.ts" />
+/// <reference path="ICompression.d.ts" />
+/// <reference path="IConcurrent.d.ts" />
+/// <reference path="IContact.d.ts" />
+/// <reference path="ICrypto.d.ts" />
+/// <reference path="IDataStream.d.ts" />
+/// <reference path="IDatabase.d.ts" />
+/// <reference path="IDesktop.d.ts" />
+/// <reference path="IDevice.d.ts" />
+/// <reference path="IDisplay.d.ts" />
+/// <reference path="IFacebook.d.ts" />
+/// <reference path="IFile.d.ts" />
+/// <reference path="IFileSystem.d.ts" />
+/// <reference path="IGeolocation.d.ts" />
+/// <reference path="IGlobalization.d.ts" />
+/// <reference path="IGooglePlus.d.ts" />
+/// <reference path="IGyroscope.d.ts" />
+/// <reference path="IImaging.d.ts" />
+/// <reference path="IInternalStorage.d.ts" />
+/// <reference path="ILifecycle.d.ts" />
+/// <reference path="ILinkedIn.d.ts" />
+/// <reference path="ILogging.d.ts" />
+/// <reference path="IMagnetometer.d.ts" />
+/// <reference path="IMail.d.ts" />
+/// <reference path="IManagement.d.ts" />
+/// <reference path="IMap.d.ts" />
+/// <reference path="IMessaging.d.ts" />
+/// <reference path="INFC.d.ts" />
+/// <reference path="INetworkInfo.d.ts" />
+/// <reference path="INetworkNaming.d.ts" />
+/// <reference path="INetworkReachability.d.ts" />
+/// <reference path="INetworkStatus.d.ts" />
+/// <reference path="INotification.d.ts" />
+/// <reference path="INotificationLocal.d.ts" />
+/// <reference path="IOAuth.d.ts" />
+/// <reference path="IOCR.d.ts" />
+/// <reference path="IOS.d.ts" />
+/// <reference path="IOpenId.d.ts" />
+/// <reference path="IPrinting.d.ts" />
+/// <reference path="IProximity.d.ts" />
+/// <reference path="IQRCode.d.ts" />
+/// <reference path="IRSS.d.ts" />
+/// <reference path="IRuntime.d.ts" />
+/// <reference path="ISecurity.d.ts" />
+/// <reference path="IService.d.ts" />
+/// <reference path="ISettings.d.ts" />
+/// <reference path="ISocket.d.ts" />
+/// <reference path="IStore.d.ts" />
+/// <reference path="ITelephony.d.ts" />
+/// <reference path="ITimer.d.ts" />
+/// <reference path="ITwitter.d.ts" />
+/// <reference path="IUI.d.ts" />
+/// <reference path="IUpdate.d.ts" />
+/// <reference path="IVibration.d.ts" />
+/// <reference path="IVideo.d.ts" />
+/// <reference path="IWallet.d.ts" />
+/// <reference path="IXML.d.ts" />
+/**
+--| ADAPTIVE RUNTIME PLATFORM |----------------------------------------------------------------------------------------
+
+(C) Copyright 2013-2015 Carlos Lozano Diez t/a Adaptive.me <http://adaptive.me>.
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 . Unless required by appli-
+-cable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,  WITHOUT
+WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the  License  for the specific language governing
+permissions and limitations under the License.
+
+Original author:
+
+    * Carlos Lozano Diez
+            <http://github.com/carloslozano>
+            <http://twitter.com/adaptivecoder>
+            <mailto:carlos@adaptive.me>
+
+Contributors:
+
+    * Ferran Vila Conesa
+             <http://github.com/fnva>
+             <http://twitter.com/ferran_vila>
+             <mailto:ferran.vila.conesa@gmail.com>
+
+    * See source code files for contributors.
+
+Release:
+
+    * @version v2.0.3
+
+-------------------------------------------| aut inveniam viam aut faciam |--------------------------------------------
+*/
+declare module Adaptive {
+}
