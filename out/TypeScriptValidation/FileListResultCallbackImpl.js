758ee51cbc832f96df6333113178e002515d524c
Carlos Lozano <carlos@adaptive.me>
2015-01-09 10:21:10 +0100

    TypeScript - File callbacks.

---
+++ out/TypeScriptValidation/FileListResultCallbackImpl.js
@@ -0,0 +1,90 @@
+/**
+--| ADAPTIVE RUNTIME PLATFORM |----------------------------------------------------------------------------------------
+
+(C) Copyright 2013-2015 Carlos Lozano Diez t/a Adaptive.me <http://adaptive.me>.
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the
+License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0 . Unless required by appli-
+-cable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,  WITHOUT
+WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the  License  for the specific language governing
+permissions and limitations under the License.
+
+Original author:
+
+    * Carlos Lozano Diez
+            <http://github.com/carloslozano>
+            <http://twitter.com/adaptivecoder>
+            <mailto:carlos@adaptive.me>
+
+Contributors:
+
+    * Ferran Vila Conesa
+             <http://github.com/fnva>
+             <http://twitter.com/ferran_vila>
+             <mailto:ferran.vila.conesa@gmail.com>
+
+    * See source code files for contributors.
+
+Release:
+
+    * @version v2.0.3
+
+-------------------------------------------| aut inveniam viam aut faciam |--------------------------------------------
+*/
+var __extends = this.__extends || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+///<reference path="BaseCallbackImpl.ts"/>
+///<reference path="FileDescriptor.ts"/>
+///<reference path="IFileListResultCallback.ts"/>
+///<reference path="IFileListResultCallbackError.ts"/>
+///<reference path="IFileListResultCallbackWarning.ts"/>
+var Adaptive;
+(function (Adaptive) {
+    /**
+       Interface for Managing the File result operations
+       Auto-generated implementation of IFileListResultCallback specification.
+    */
+    var FileListResultCallbackImpl = (function (_super) {
+        __extends(FileListResultCallbackImpl, _super);
+        /**
+           Constructor with callback id.
+
+           @param id  The id of the callback.
+        */
+        function FileListResultCallbackImpl(id) {
+            _super.call(this, id);
+        }
+        /**
+           On error result of a file operation.
+
+           @param error Error processing the request.
+           @since ARP1.0
+        */
+        FileListResultCallbackImpl.prototype.onError = function (error) {
+        };
+        /**
+           On correct result of a file operation.
+
+           @param files Array of resulting files/folders.
+           @since ARP1.0
+        */
+        FileListResultCallbackImpl.prototype.onResult = function (files) {
+        };
+        /**
+           On partial result of a file operation, containing a warning.
+
+           @param files   Array of resulting files/folders.
+           @param warning Warning condition encountered.
+           @since ARP1.0
+        */
+        FileListResultCallbackImpl.prototype.onWarning = function (files, warning) {
+        };
+        return FileListResultCallbackImpl;
+    })(Adaptive.BaseCallbackImpl);
+    Adaptive.FileListResultCallbackImpl = FileListResultCallbackImpl;
+})(Adaptive || (Adaptive = {}));
+//# sourceMappingURL=FileListResultCallbackImpl.js.map
