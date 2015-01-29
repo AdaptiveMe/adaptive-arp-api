7d7a765012b9a62fe08556511d9901dc325b8b8c
Carlos Lozano <carlos@adaptive.me>
2015-01-30 00:18:18 +0100

    API v2.1.1 Specification

--- out/CSharpValidation/CSharpValidation/Sources/Adaptive.Arp.Api/ServiceHeader.cs
+++ out/CSharpValidation/CSharpValidation/Sources/Adaptive.Arp.Api/ServiceHeader.cs
@@ -27,7 +27,7 @@ Contributors:
 
 Release:
 
-    * @version v2.0.8
+    * @version v2.1.0
 
 -------------------------------------------| aut inveniam viam aut faciam |--------------------------------------------
 */
