4e5b8dc18639094a5149807e1742b409fb14640f
Carlos Lozano <carlos@adaptive.me>
2015-02-25 19:54:59 +0100

    Fetch tools.

---
+++ generate.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+HOME_DIR=`pwd`
+
+if [ "$TRAVIS_PULL_REQUEST" = "false" ]
+then
+    echo "Forged with ♥ in Barcelona · © 2013 - 2015 adaptive.me / Carlos Lozano Diez."
+    echo
+    API_VERSION=`git describe --abbrev=0`
+
+    rm -rf $HOME_DIR/generate
+    mkdir -p $HOME_DIR/generate
+    cd $HOME_DIR/generate
+
+    git clone --depth 1 https://carloslozano:$GITHUB_TOKEN@github.com/AdaptiveMe/adaptive-arp-tools.git
+
+    echo $HOME_DIR
+    echo $API_VERSION
+fi
+
